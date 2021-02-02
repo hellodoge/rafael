8075778687f25fc469bf11fb6405e3adc53b49e1
waldemar <hellodoge@tuta.io>
2021-02-02 22:25:05 +0300

    ctrl_lambda, ctrl_run_lambda added

--- src/include/execution.h
+++ src/include/execution.h
@@ -10,4 +10,6 @@ arg_t *execute(const stm_t *stm);
 
 arg_t *execute_inner_stm(fnc_ptr_t function, const arg_t *args, bool pass_next);
 
+arg_t *macro_processor(const arg_t *macro_def, const arg_t *args); //lambdas
+
 #endif //RAFAEL_EXECUTION_H
