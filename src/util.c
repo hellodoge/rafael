7d60f79460c661ead7549fb5f13bfd6d9d9db63b
waldemar <hellodoge@tuta.io>
2021-02-04 17:28:14 +0300

    add function get_n_chars_from_utf8_str

--- src/util.c
+++ src/util.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <stdint.h>
 
 #define DEFAULT_BUFFER_SIZE 16
 
@@ -39,6 +41,29 @@ char *get_unlimited_input(char *buffer, bool *eof_flag) {
 }
 
 #undef DEFAULT_BUFFER_SIZE
+
+char *get_n_chars_from_utf_8_str(const char *string, size_t n) {
+	/// returns NULL is string is not valid UTF-8
+	assert(string != NULL);
+	size_t size = 0;
+	for (size_t i = 0; i < n; i++) {
+		const uint8_t *bytes = (uint8_t *) string + size;
+		uint8_t mask = 1u << 6u | 1u << 7u;
+		size_t size_of_char = 1;
+		while ((*bytes & mask) == mask) {
+			mask = (mask >> 1u) | 1u << 7u;
+			size_of_char++;
+		}
+		for (size_t j = 1; j < size_of_char; j++) {
+			if ((*(bytes + j) & (1u << 6u | 1u << 7u)) != 1u << 7u)
+				return NULL;
+		}
+		if (size_of_char == 1 && (*bytes & 1u << 7u) == 1u << 7u)
+			return NULL;
+		size += size_of_char;
+	}
+	return strndup(string, size);
+}
 
 void print_error(const char *message) {
 	fprintf(stderr, "Error! %s\n", message);
