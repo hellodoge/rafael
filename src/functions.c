94218cb68b1f8c36c5f57cc04753d3a1968568f1
waldemar <hellodoge@tuta.io>
2021-02-05 11:47:55 +0300

    (fnc_to_real) add type check

--- src/functions.c
+++ src/functions.c
@@ -642,11 +642,15 @@ arg_t *fnc_to_int(const arg_t *args) {
 
 arg_t *fnc_to_real(const arg_t *args) {
 	arg_t *ret = NULL;
+	if (!args_match_pattern(args, F_NUMBER | T_STRING | F_MULTIPLE | F_OPTIONAL, F_END)) {
+		EXCEPTION(ret, "real: invalid arguments");
+	}
 	for (const arg_t *arg = args; arg != NULL; arg = arg->next) {
 		arg_t *current = init_arg(T_REAL);
 		current->floating = get_real(arg);
 		add_arg(&ret, current);
 	}
+err:
 	return ret;
 }
 
