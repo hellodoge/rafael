b1a882a7afa92779ad4aec30e559e7c3951a9d40
waldemar <hellodoge@tuta.io>
2021-02-06 19:48:21 +0300

    add EXCEPTION_IF_UNKNOWN_KEYWORDS

--- src/include/terminate.h
+++ src/include/terminate.h
@@ -15,4 +15,11 @@ if (arg->type & F_TERMINATE) { \
 	goto err; \
 }
 
+#define EXCEPTION_IF_UNKNOWN_KEYWORDS(name, ret, args, ...) \
+{ \
+	const char *unknown = find_unknown_keywords(args, ##__VA_ARGS__, NULL); \
+	if (unknown != NULL) { \
+		EXCEPTION(ret, "%s: unknown keyword `%s`", name, unknown); \
+	} \
+}
 #endif //RAFAEL_TERMINATE_H
